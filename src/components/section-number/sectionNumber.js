c16bc9e7e28a7f4686e78ff53aa227c9977c203c
jelarson <jesslarson93@gmail.com>
2020-07-07 13:09:26 -0600

    wrong answers show as red, correct as green

--- src/components/section-number/sectionNumber.js
+++ src/components/section-number/sectionNumber.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import { css } from '@emotion/core'
 
 import { SelectedContext } from '../context/context'
@@ -11,25 +11,63 @@ const numberCss = css`
   width: 45px;
   height: 45px;
 `
+const correctCss = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: black 1px solid;
+  width: 45px;
+  height: 45px;
+  color: green;
+`
+const wrongCss = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: black 1px solid;
+  width: 45px;
+  height: 45px;
+  color: white;
+  background-color: red;
+`
 
 export default function SectionNumber(props) {
   const { numberVal, numberVisible } = props
   const [numVisible, setNumVisible] = useState(numberVisible)
   const { selected } = useContext(SelectedContext)
+  // const [rightAnswer, setRightAnswer] = useState(false)
+  // const [wrongAnswer, setWrongAnswer] = useState(false)
+  const [wrongAnswerVal, setWrongAnswerVal] = useState(0)
+  const [boxStyle, setBoxStyle] = useState(numberCss)
 
   function checkAnswer() {
     console.log(numberVal)
     if (selected.selectedNum === numberVal) {
       console.log('you got it right!')
-      setNumVisible(true)
+      setBoxStyle(correctCss)
+      // setRightAnswer(true)
     } else {
       console.log('you got it wrong')
+      setWrongAnswerVal(selected.selectedNum)
+      setBoxStyle(wrongCss)
     }
   }
 
+  useEffect(() => {
+    if (numVisible && selected.selectedNum === numberVal) {
+      // increase font size
+    }
+  }, [numVisible, selected.selectedNum])
+
+  useEffect(() => {
+    if (boxStyle === correctCss) {
+      setNumVisible(true)
+    }
+  }, [boxStyle])
+
   return (
-    <div css={numberCss} onClick={checkAnswer}>
-      {numVisible ? numberVal : ' '}
+    <div css={boxStyle} onClick={checkAnswer}>
+      {numVisible ? numberVal : boxStyle === wrongCss ? wrongAnswerVal : ' '}
     </div>
   )
 }
